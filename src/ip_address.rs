c0f169f54ccc2b052b5bed04f17cf19c4648a107
timdegen <104884878+timdegen@users.noreply.github.com>
2023-05-13 00:31:25 +0100

    Implement getting IP Address

--- src/ip_address.rs
+++ src/ip_address.rs
@@ -1,6 +1,19 @@
-use std::net::SocketAddr;
+extern crate rocket;
+
+use rocket::serde::Serialize;
+use rocket::serde::json::Json;
+use rocket_client_addr::ClientRealAddr;
 
 #[get("/ip_address")]
-pub async fn get_ip_address(addr: SocketAddr) -> String {
-    return format!("{}", addr.ip());
+pub fn get_ip_address(client_addr: &ClientRealAddr) -> Json<IpResponse> {
+    Json(IpResponse { 
+        ipv4: client_addr.get_ipv4_string().unwrap(),
+        ipv6: client_addr.get_ipv6_string(),
+    })   
+}
+
+#[derive(Serialize)]
+pub struct IpResponse {
+    ipv4: String,
+    ipv6: String,
 }
