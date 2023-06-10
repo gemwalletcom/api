2e6b7eab466f1bd4cc77bf97a7a7db71c05aef90
timdegen <104884878+timdegen@users.noreply.github.com>
2023-06-10 04:58:52 +0100

    Use std::net::IpAddr

--- client/src/status.rs
+++ client/src/status.rs
@@ -5,10 +5,16 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use rocket_client_addr::ClientRealAddr;
 
 #[get("/")]
-pub fn get_status(client_addr: &ClientRealAddr) -> Json<Status> {
+pub fn get_status(
+    ip: std::net::IpAddr,
+    ip_socket: std::net::SocketAddr,
+    client_addr: &ClientRealAddr
+) -> Json<Status> {
    Json(Status { 
         time: get_epoch_ms(),
         ipv4: client_addr.get_ipv4_string().unwrap(),
+        ipv4_v2: ip.to_string(),
+        ipv4_v3: ip_socket.to_string(),
         ipv6: client_addr.get_ipv6_string(),
     })
 }
@@ -24,6 +30,8 @@ fn get_epoch_ms() -> u128 {
 pub struct Status {
     time: u128,
     ipv4: String,
+    ipv4_v2: String,
+    ipv4_v3: String,
     ipv6: String,
 }
 
