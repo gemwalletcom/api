35f870d07be5833bb3991d07859e632b1fea1e9d
timdegen <104884878+timdegen@users.noreply.github.com>
2023-06-23 18:31:05 +0100

    Add app configuration

--- client/src/config_client.rs
+++ client/src/config_client.rs
@@ -10,9 +10,21 @@ const CONFIG_TOKENLIST_PREFIX: &str = "config:tokenlists:";
 #[serde(rename_all = "camelCase")]
 pub struct ConfigResponse {
     pub nodes_version: i32,
+    pub app: App,
     pub fiat_assets_version: i32,
     pub token_lists_version: i32,
     pub token_lists: Vec<TokenListVersion>
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AppVersion {
+    pub version: String
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct App {
+    pub ios: AppVersion,
+    pub android: AppVersion,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -36,15 +48,22 @@ impl Client {
     }
 
     pub async fn get_config(&mut self) -> Result<ConfigResponse, Box<dyn Error>> {
-        let token_lists: Vec<TokenListVersion> = self.store.get_values(CONFIG_TOKENLIST_PREFIX).await.unwrap();
+        let token_lists: Vec<TokenListVersion> = self.store.get_values(CONFIG_TOKENLIST_PREFIX).await.unwrap_or_default();
         let token_lists_version: i32 = token_lists
             .iter()
             .map(|token_list| token_list.version)
             .sum();
         
+        let ios_version = self.store.get_value("config:ios_version").await.unwrap();
+        let android_version = self.store.get_value("config:android_version").await.unwrap();
+
         let response = ConfigResponse{
             //TODO fetch fiat assets version from db
             nodes_version: 1,
+            app: App { 
+                ios: ios_version,
+                android: android_version,
+            },
             fiat_assets_version: 1,
             token_lists_version,
             token_lists
