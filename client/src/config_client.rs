1b7ed195fad78560ad4f999fdeac015f5da14661
timdegen <104884878+timdegen@users.noreply.github.com>
2023-06-27 05:34:33 +0100

    Improve config response

--- client/src/config_client.rs
+++ client/src/config_client.rs
@@ -9,26 +9,44 @@ const CONFIG_TOKENLIST_PREFIX: &str = "config:tokenlists:";
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ConfigResponse {
-    pub nodes_version: i32,
     pub app: App,
-    pub fiat_assets_version: i32,
-    pub token_lists_version: i32,
-    pub token_lists: Vec<TokenListVersion>
+    pub versions: Versions,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Versions {
+    pub nodes: i32,
+    pub fiat_assets: i32,
+    pub token_lists: i32,
+    pub token_lists_chains: Vec<TokenListVersion>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct AppVersion {
-    pub version: String,
-    pub beta_version: String,
-    pub alpha_version: String,
+    pub production: String,
+    pub beta: String,
+    pub alpha: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AppAndroid {
+    pub version: AppVersion
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AppiOS {
+    pub version: AppVersion
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct App {
-    pub ios: AppVersion,
-    pub android: AppVersion,
+    pub ios: AppiOS,
+    pub android: AppAndroid,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -52,8 +70,8 @@ impl Client {
     }
 
     pub async fn get_config(&mut self) -> Result<ConfigResponse, Box<dyn Error>> {
-        let token_lists: Vec<TokenListVersion> = self.store.get_values(CONFIG_TOKENLIST_PREFIX).await.unwrap_or_default();
-        let token_lists_version: i32 = token_lists
+        let token_lists_chains: Vec<TokenListVersion> = self.store.get_values(CONFIG_TOKENLIST_PREFIX).await.unwrap_or_default();
+        let token_lists: i32 = token_lists_chains
             .iter()
             .map(|token_list| token_list.version)
             .sum();
@@ -61,11 +79,13 @@ impl Client {
 
         let response = ConfigResponse{
             //TODO fetch fiat assets version from db
-            nodes_version: 1,
             app,
-            fiat_assets_version: 1,
-            token_lists_version,
-            token_lists
+            versions: Versions { 
+                nodes: 1, 
+                fiat_assets: 1, 
+                token_lists,
+                token_lists_chains,
+            }
         };
         return Ok(response)
     }
